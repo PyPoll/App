f44c77bbc6106f62d5925ac87c056317f07d8b5e
FurWaz <fur.waz06@gmail.com>
2024-08-06 23:56:08 +0200

    Updated deep-link properties

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.furwaz.Pypoll',
+  appId: 'com.furwaz.pypoll',
   appName: 'Pypoll',
   webDir: 'dist'
 };
